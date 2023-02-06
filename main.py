4d2d87457ad70ea6f9d6f14c6971779e5ff7eb34
Ls Today <levine23@lsrhs.net>
2023-02-06 14:37:59 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import time
+def rep(filename,search,replace):
+    data = ""
+    with open(filename, 'r') as file:
+        data = file.read()
+        data = data.replace(search,replace)
+    with open(filename, 'w') as file:
+        file.write(data)
+
+filepath = input("path:")
+sname = input("service name:")
+serversn = input("server names:")
+
+rep("sname.ini","sname",sname)
+os.rename("sname.ini",f"{sname}.ini")
+
+rep("servicefile.txt","filepath",filepath)
+rep("servicefile.txt","sname",sname)
+
+os.rename("servicefile.txt",f"/etc/systemd/system/{sname}.service")
+os.system(f"sudo systemctl start {sname}")
+time.sleep(1)
+os.system(f"sudo systemctl enable {sname}")
+
+rep("nginx.txt","sname",sname)
+rep("nginx.txt","filepath",filepath)
+rep("nginx.txt","serversn",serversn)
+
+dat = ""
+with open("nginx.txt",'r') as file:
+    dat = file.read()
+with open("nginx2.txt",'w') as file:
+    file.write(dat)
+
+os.rename("nginx.txt",f"/etc/nginx/sites-available/{sname}")
+os.rename("nginx2.txt",f"/etc/nginx/sites-enabled/{sname}")
+
+os.system("sudo systemctl restart nginx")
+print("done")
+exit(0)
